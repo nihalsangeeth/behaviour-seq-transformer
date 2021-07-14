1c5dddbfbe3927576ee8bf5fa79428745c231c7b
nihalsangeeth <nihalsangeeth4@gmail.com>
2021-07-14 15:25:58 +0530

    Added BST model class

--- bst/layers.py
+++ bst/layers.py
@@ -110,3 +110,47 @@ class Encoder(nn.Module):
         for cell in self.stack:
             x = cell(self.dropout(x), mask)
         return x
+
+
+class Model(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.item_embed = nn.Embedding(num_embeddings=config['item_embed']['num_embeddings'],
+                                       embedding_dim=config['item_embed']['embedding_dim'],
+                                       sparse=config['item_embed']['sparse'],
+                                       padding_idx=config['item_embed']['padding_idx'])
+
+        self.pos_embedding = self.pos_embedding_sinusoidal(config['max_seq_len'],
+                                                           config['item_embed']['embedding_dim'],
+                                                           config['cuda'])
+        self.context_embeddings = nn.ModuleList([nn.Embedding(num_embeddings=feat['num_embeddings'],
+                                                              embedding_dim=feat['embedding_dim'],
+                                                              sparse=feat['sparse'],
+                                                              padding_idx=feat['padding_idx'])
+                                                 for feat in config['context_features']])
+
+        self.encoder = Encoder(input_size=config['trans']['input_size'],
+                               hidden_size=config['trans']['hidden_size'],
+                               n_layers=config['trans']['n_layers'],
+                               n_heads=config['trans']['n_heads'])
+
+        mlp_input_size = config['trans']['input_size'] + sum(
+            [feat['embedding_dim'] for feat in config['context_features']])
+
+        self.mlp = nn.Sequential(nn.Linear(mlp_input_size, 1024),
+                                 nn.LeakyReLU(),
+                                 nn.Linear(1024, config['item_embed']['num_embeddings'])
+                                 )
+
+    def forward(self, x, context):
+        targets = x[..., -1:].long()
+        enc_mask = self.get_mask(x)
+        item_embed = self.item_embed(x.long()) * np.sqrt(self.config['item_embed']['embedding_dim'])
+        agg_encoding = torch.sum(self.encoder(item_embed + self.pos_embedding[:x.size(1), :], mask=enc_mask), dim=1)
+        context_embs = torch.tensor([]).to(x.device)
+        for emb, feat in zip(self.context_embeddings, context):
+            context_embs = torch.cat([context_embs, emb(feat)], dim=1)
+        output = self.mlp(torch.cat([agg_encoding, context_embs], dim=1))
+        return output, targets
+
